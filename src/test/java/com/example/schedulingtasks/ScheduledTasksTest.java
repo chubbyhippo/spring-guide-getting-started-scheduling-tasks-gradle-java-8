8d5076fce6ad5998a7f0f1b144336a13311525f8
ch <mkmsacc@outlook.com>
2020-10-31 19:06:47 +0700

    add ScheduledTasksTest

---
+++ src/test/java/com/example/schedulingtasks/ScheduledTasksTest.java
@@ -0,0 +1,24 @@
+package com.example.schedulingtasks;
+
+import static org.awaitility.Awaitility.await;
+import static org.mockito.Mockito.atLeast;
+import static org.mockito.Mockito.verify;
+
+import org.awaitility.Duration;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+
+@SpringBootTest
+class ScheduledTasksTest extends ScheduledTasks {
+
+	@SpyBean
+	ScheduledTasks tasks;
+
+	@Test
+	public void reportCurrentTime() {
+		await().atMost(Duration.TEN_SECONDS).untilAsserted(() -> {
+			verify(tasks, atLeast(2)).reportCurrentTime();
+		});
+	}
+}
