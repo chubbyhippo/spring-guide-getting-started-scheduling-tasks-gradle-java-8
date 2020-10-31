52950dfd0f150244ed342d4ecf9478de3ccb6550
ch <mkmsacc@outlook.com>
2020-10-31 18:28:10 +0700

    add @Component

--- src/main/java/com/example/schedulingtasks/ScheduledTasks.java
+++ src/main/java/com/example/schedulingtasks/ScheduledTasks.java
@@ -6,7 +6,9 @@ import java.util.Date;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ScheduledTasks {
 
 	private static final Logger log = LoggerFactory
